ede7eca8b7f8250b87997b95de035a5a31df4809
Jose-Prince <joseprince148@gmail.com>
2025-04-23 18:08:31 -0600

    add: endpoints description added

--- backend/Program.cs
+++ backend/Program.cs
@@ -3,4 +3,24 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+//ENDPOINTS:
+//CREATE:
+//- Crear un pedido
+//- Crear reseña asociada 
+//READ:
+//- Obtener restaurantes (Nombre, Foto referencia, Calificación, size, page)
+//- Obtener los diferentes tipos de comida de los restaurantes (sin repeticiones)
+//- Obtener ofertas (Nombre del artículo, Precio total, precio base, nomnre de restaurante, descuento, Foto de artículo, size, page)
+//- Obtener restaurante por nombre (Nombre, Foto referencia, Calificación)
+//- Obtener los elementos del carrito de ordenes que esten en estado "no ordenado"
+//- Obtener ordenes para un cliente en estado ordenado y en camino (no se obtiene: Cliente)
+//- Obtener ordenes para un cliente en estado entregado (no se obtiene: Cliente)
+//- Obtener los datos del cliente
+//UPDATE:
+//- Añadir tarjeta a un usuario
+//- Añadir artículo a carrito (actualizar el total a pagar)
+//- Cambiar estado de la orden
+//DELETE:
+//- Quitar trajeta
+//- Eliminar una orden
 app.Run();
