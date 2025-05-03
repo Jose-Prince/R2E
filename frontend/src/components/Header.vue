6d400d79bc4b0621308b93518eab289be3565e73
Jose-Prince <joseprince148@gmail.com>
2025-05-03 13:38:53 -0600

    style: header recreated

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,15 +1,21 @@
 <template>
-    <v-app-bar :elevation="2">
+    <v-app-bar :elevation="2" class="px-4"> <!-- Padding lateral -->
+        <template v-slot:prepend>
+            <v-app-bar-title>READY 2 EAT</v-app-bar-title>
+        </template>
+      <div style="display: flex; width: 100%; gap: 5vw; margin-left: 1rem;">
         <v-text-field
-            density="compact"
-            hide-details
-            single-line
-            variant="outlined"
-            placeholder="Buscar..."
-        />    
-        
+          density="compact"
+          hide-details
+          single-line
+          variant="outlined"
+          placeholder="Search restaurant..."
+          class="flex-grow-1"
+        ></v-text-field>
+        <v-icon-btn color="primary" icon="$vuetify" variant="outlined"></v-icon-btn>
+      </div>
     </v-app-bar>
-</template>
+  </template>
 
 <script>
 export default  {
