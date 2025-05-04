6e0fe27bb53e1671dda30dc7621015ca556b856d
Jose-Prince <joseprince148@gmail.com>
2025-05-04 12:00:12 -0600

    Feat: controller endpoints

---
+++ frontend/src/controller/controller.js
@@ -0,0 +1,36 @@
+export async function getUser(id) {
+  try {
+    const response = await fetch(`http://localhost:5125/user/${id}`, {
+      method: 'GET',
+      headers: { "Content-Type": "application/json" },
+    })
+
+    if (!response.ok) {
+      throw new Error(`Error in request: ${response.status}`)
+    }
+
+    return await response.json()
+  } catch (error) {
+    console.error("Error in data", error)
+    throw error
+  }
+}
+
+export async function updateUser(id, updatedUser) {
+  try {
+    const response = await fetch(`http://localhost:5125/users/${id}`, {
+      method: 'PATCH',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(updatedUser)
+    })
+
+    if (!response.ok) {
+      throw new Error(`Error updating: ${response.statusText}`)
+    }
+
+    return await response.json()
+  } catch (error) {
+    console.error("Error updating user:", error)
+    throw error
+  }
+}
